0b00d29d3d7967f1e0fee887fd7e939fbd834ab7
artalexandra <alexandraartyushkova@yandex.ru>
2026-07-05 15:35:25 +0300

    feat: added Label

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -3,6 +3,8 @@
 import { ChangeEvent } from 'react';
 import { Input as AntInput } from 'antd';
 
+import Label from '@bdt/shared/ui/Label';
+
 import style from './Input.module.scss';
 
 type TInputStatus = 'warning' | 'error' | 'success' | 'validating';
@@ -19,12 +21,13 @@ interface IInputProps {
     value?: string;
     type?: TInputType;
     status?: TInputStatus;
+    label?: string;
     className?: string;
 
     onChange: (value: string) => void;
 };
 
-function Input({ value, status, type, className, onChange }: IInputProps) {
+function Input({ value, status, type, label, className, onChange }: IInputProps) {
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         onChange(e.target.value);
@@ -32,11 +35,13 @@ function Input({ value, status, type, className, onChange }: IInputProps) {
 
     if (type === INPUT_TYPE.PASSWORD) {
         return <div className={className}>
-            <AntInput.Password status={status} type={type} value={value} onChange={handleChange} className={style.input} />;
+            { label && <Label text={label} /> }
+            <AntInput.Password status={status} type={type} value={value} onChange={handleChange} className={style.input} />
         </div>;
     }
 
     return <div className={className}>
+        { label && <Label text={label} /> }
         <AntInput status={status} type={type} value={value} onChange={handleChange} className={style.input} />
     </div>;
 }
