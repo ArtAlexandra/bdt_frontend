7d15a302b91997d980e145b01020c1310158fe0c
artalexandra <alexandraartyushkova@yandex.ru>
2026-08-09 17:02:27 +0300

    refactor: changed appenvironment & env.example

--- src/shared/config/AppEnvironment.ts
+++ src/shared/config/AppEnvironment.ts
@@ -1,4 +1,4 @@
-export const PUBLIC_URL = process.env.NEXT_PUBLIC_URL || '';
+export const PUBLIC_URL = process.env.PUBLIC_URL || '';
 
 export const BDT_EMAIL = 'info@bdt.ru';
 export const BDT_PHONE = '+7(495)788-57-67';
@@ -13,10 +13,10 @@ export const VK_SERVICE_KEY = process.env.VK_SERVICE_KEY || '';
 export const VK_VERSION = '5.199';
 export const VK_PUBLIC_WALL_URL = 'https://vk.com/wall';
 
-export const PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001';
+export const PUBLIC_API_URL = process.env.API_URL ?? 'http://localhost:3001';
 
 export const ADMIN_SECRET_KEY = process.env.ADMIN_SECRET_KEY ?? '';
-export const ADMIN_VERIFICATION_KEY = process.env.NEXT_PUBLIC_ADMIN_VERIFICATION_KEY ?? '';
+export const ADMIN_VERIFICATION_KEY = process.env.ADMIN_VERIFICATION_KEY ?? '';
 
 export const VK_LOGO_URL = '/image/vk_logo.png';
 export const LOGO_COLOR_URL = '/image/logo-color.jpg';
